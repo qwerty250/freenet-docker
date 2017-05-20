e4038482c615dc9b2a3508d93e53e3844837d97f
qwerty250 <qiyue2001@gmail.com>
2017-05-20 10:10:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d --name freenet --restart=always --net=host -v $PWD/data:/freenet/data nextgens/freenet:latest
+docker run -d --name freenet --restart=always --net=host -v $PWD/data:/freenet/data qwerty250/freenet-docker:latest
